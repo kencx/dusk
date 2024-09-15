e00f4fce3f470e214e7b2577fba78af31c14ecff
kencx <kennethcheo@pm.me>
2024-09-15 12:16:03 +0800

    Close modal by clicking background

--- ui/partials/modal_templ.go
+++ ui/partials/modal_templ.go
@@ -26,7 +26,7 @@ func ModalButton(attrs templ.Attributes) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<button hx-target=\"#modal-content\"")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<a role=\"button\" hx-target=\"#modal-content\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -42,7 +42,7 @@ func ModalButton(attrs templ.Attributes) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</button>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</a>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
--- ui/static/js/actions.js
+++ ui/static/js/actions.js
@@ -1,15 +1,14 @@
 document.body.addEventListener("openModal", function(event) {
     const modalDiv = document.querySelector("#modal-content")
     const modalDialog = modalDiv.querySelector("dialog")
-    // const closeModalBtn = modalDiv.querySelector('button[aria-label="close"]#modal-close-btn')
     const cancelModalBtn = modalDiv.querySelector("#modal-cancel-btn")
 
     const closeModal = function() {
         modalDialog.removeAttribute("open")
     }
 
-    // closeModalBtn.addEventListener("click", closeModal);
     cancelModalBtn.addEventListener("click", closeModal);
+    modalDialog.addEventListener("click", closeModal);
 
     // close on Esc
     document.addEventListener("keydown", function(e) {
